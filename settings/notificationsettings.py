de8624f449fc7c64bb59236fdaabf6b28903dcc5
Lavender Starlight <56975081+faithvoid@users.noreply.github.com>
2024-11-12 11:36:02 -0500

    Add files via upload

--- settings/notificationsettings.py
+++ settings/notificationsettings.py
@@ -7,10 +7,8 @@ def main():
     dialog = xbmcgui.Dialog()
     feeds = [
         ("...", "RunScript(Q:\\scripts\\Cortana Server Browser\\settings\\settings.py)"),
-        ("Enable Insignia Notifications On Startup", "RunScript(Q:\\scripts\\Cortana Server Browser\\settings\\insigniastartup.py)"),
-        ("Disable Insignia Notifications On Startup", "RunScript(Q:\\scripts\\Cortana Server Browser\\settings\\noinsigniastartup.py)"),
-        ("Enable XLink Kai Notifications On Startup", "RunScript(Q:\\scripts\\Cortana Server Browser\\settings\\xlinkstartup.py)"),
-        ("Disable XLink Kai Notifications On Startup", "RunScript(Q:\\scripts\\Cortana Server Browser\\settings\\noxlinkstartup.py)"),
+        ("Enable / Disable Insignia Notifications On Startup", "RunScript(Q:\\scripts\\Cortana Server Browser\\settings\\startup\\autoexec-insignia.py)"),
+        ("Enable / Disable XLink Kai Notifications On Startup", "RunScript(Q:\\scripts\\Cortana Server Browser\\settings\\startup\\autoexec-xlinkkai.py)"),
     ]
     
     feed_list = [name for name, _ in feeds]
@@ -20,6 +18,19 @@ def main():
         name, url = feeds[selected]
         if "RunScript" in url:
             xbmc.executebuiltin(url)
+
+def display_feed_items(dialog, channel, name):
+    items = []
+    for item in channel.findall('item'):
+        title_elem = item.find('title')
+        description_elem = item.find('description')  # Assuming description is the correct tag for item description
+        
+        item_title = title_elem.text if title_elem is not None else u"Not Available"
+        item_description = description_elem.text if description_elem is not None else u""
+        
+        items.append(u"{} {}".format(item_title, item_description))
+    
+    dialog.select(u"Cortana Server Browser - {}".format(name), items)
 
 if __name__ == '__main__':
     main()
