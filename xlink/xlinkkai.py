6346aea2e346d55ea3979bd68ab0a57a9446ea4c
Lavender Starlight <56975081+faithvoid@users.noreply.github.com>
2024-11-12 10:55:00 -0500

    Update xlinkkai.py

--- xlink/xlinkkai.py
+++ xlink/xlinkkai.py
@@ -23,18 +23,5 @@ def main():
         if "RunScript" in url:
             xbmc.executebuiltin(url)
 
-def display_feed_items(dialog, channel, name):
-    items = []
-    for item in channel.findall('item'):
-        title_elem = item.find('title')
-        description_elem = item.find('description')  # Assuming description is the correct tag for item description
-        
-        item_title = title_elem.text if title_elem is not None else u"Not Available"
-        item_description = description_elem.text if description_elem is not None else u""
-        
-        items.append(u"{} {}".format(item_title, item_description))
-    
-    dialog.select(u"Cortana Server Browser - {}".format(name), items)
-
 if __name__ == '__main__':
     main()
